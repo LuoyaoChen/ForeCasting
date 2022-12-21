27e1ba3f1f970bc8c9ee7ea75ade60e946e8014f
chen5301 <chen5301@umn.edu>
2022-12-21 00:32:27 -0500

    homepage fixed and app

--- app.py
+++ app.py
@@ -1,24 +1,50 @@
+from flask import Flask, redirect, url_for, render_template, request, abort
+import mysql.connector
+from mysql.connector import errorcode
 
-from flask import Flask, render_template, request
-
-
-
+from connection import connect_to_cursor
 app = Flask(__name__)
 
+@app.route('/')
+def homepage():
+    table_names = connect_to_cursor(task='get_table_names')
+    return render_template("homepage.html", 
+                           table_names=table_names
+                           )
 
-@app.route('/', methods=['GET', 'POST'])
-def insert():
-    return render_template('insert.html')
 
+@app.route('/tables/<table_name>/')
+def one_table(table_name):
+    table_content = connect_to_cursor(one_table_name=table_name, task="get_table_contents")
+    return render_template("one_table.html",
+                           table_name = table_name,
+                        table_content = table_content)
+
+
+
+
+
+
+
+# '''
+# take in input from usr 
+# '''
+# @app.route('/')
+# def student():
+#    return render_template('student.html')
+
+# '''
+# put into result table
+# '''
+# @app.route('/result',methods = ['POST', 'GET'])
+# def result():
+#    if request.method == 'POST':
+#       result = request.form
+#       return render_template("result.html",result = result)
 
 # @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     return render_template('delete.html')
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     return render_template('update.html')
+# def insert():
+#     return render_template('insert.html')
 
 
 
---
+++ connection.py
@@ -0,0 +1,72 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+def connect_to_cursor(one_table_name=None, task='get_table_contents'):
+  # Obtain connection string information from the portal
+  config = {
+      'host': 'diabetesforecast.mysql.database.azure.com',
+      'user': 'cly',
+      'password': 'DMSFALL2022%-',
+      'database': 'chronic',
+      'client_flags': [mysql.connector.ClientFlag.SSL],
+      # 'client_flags': [2048],
+      'ssl_ca': './DigiCertGlobalRootCA.crt.pem'
+  }
+
+  # Construct connection string
+
+  try:
+    conn = mysql.connector.connect(**config)
+    print("Connection established")
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+      print("Something is wrong with the user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+      print("Database does not exist")
+    else:
+      print(err)
+  else:
+    cursor = conn.cursor()
+
+    ################## Drop previous table of same name if one exists ###################
+    cursor.execute("DROP TABLE IF EXISTS inventory;")
+    print("Finished dropping table (if existed).")
+
+    ################### Create table ###################
+    cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER, place VARCHAR(50));")
+    print("Finished creating table.")
+
+    # Insert some data into table
+    cursor.execute("INSERT INTO inventory (name, quantity, place) VALUES (%s, %s, %s);", ("banana", 150, "US"))
+    print("Inserted",cursor.rowcount,"row(s) of data.")
+    cursor.execute("INSERT INTO inventory (name, quantity, place) VALUES (%s, %s, %s);", ("orange", 154, "US"))
+    print("Inserted",cursor.rowcount,"row(s) of data.")
+    cursor.execute("INSERT INTO inventory (name, quantity, place) VALUES (%s, %s, %s);", ("apple", 100,"US"))
+    print("Inserted",cursor.rowcount,"row(s) of data.")
+    cursor.execute(
+        "INSERT INTO inventory (name, quantity, place) VALUES (%s, %s, %s);", ("yes", 100, "US"))
+    print("Inserted", cursor.rowcount, "row(s) of data.")
+    # print(f"task = {task}")
+    if task == 'get_table_names':
+        #### get table names  #### ref = https://stackoverflow.com/questions/3556305/how-to-retrieve-table-names-in-a-mysql-database-with-python-and-mysqldb
+        cursor.execute("USE chronic")
+        cursor.execute("SHOW TABLES")
+        all_table_names = cursor.fetchall() 
+        table_names = [t[0] for t in all_table_names]
+        print(f"tables = {table_names}") ## tables = ['accounnmember', 'account', 'account_admin', 'account_alias', 'associate', 'biling_account', 'contract', 'contractbenefit', 'contractplayingrole', 'contractpremium', 'customer', 'customer_address', 'inventory', 'manager_contract', 'product', 'scores']
+        return table_names
+    elif task == 'get_table_contents':
+        # print("in task == get_table_contents")
+        cursor.execute("SELECT * FROM " + one_table_name + ";")
+        table_content = cursor.fetchall()
+        return table_content
+    
+    #### Cleanup ####
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+if __name__ =="__main__":
+  table_content = connect_to_cursor(one_table_name='inventory', task='get_table_contents')
+  for row in table_content:
+    print(row)
